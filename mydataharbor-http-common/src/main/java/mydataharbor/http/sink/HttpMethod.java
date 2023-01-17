bb57e0f8833e7f97dddb3d54bd97caa2c2c23806
xulang <xulang@doba.com>
2023-01-17 17:48:31 +0800

    2.x主框架适配

--- mydataharbor-http-common/src/main/java/mydataharbor/http/sink/HttpMethod.java
+++ mydataharbor-http-common/src/main/java/mydataharbor/http/sink/HttpMethod.java
@@ -202,32 +202,17 @@
  *    limitations under the License.
  */
 
-package mydataharbor.sink.redis;
-
-import mydataharbor.classutil.classresolver.MyDataHarborMarker;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import okhttp3.RequestBody;
-
-import java.util.Map;
+package mydataharbor.http.sink;
 
 /**
  * @auth xulang
  * @Date 2021/7/6
  **/
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class HttpSinkReq {
-
-  @MyDataHarborMarker(title = "请求方法", require = true)
-  private HttpMethod httpMethod;
-
-  @MyDataHarborMarker(title = "请求体", des = "有些请求不需要时，置为null", require = false)
-  private RequestBody requestBody;
-
-  @MyDataHarborMarker(title = "头信息", require = true)
-  private Map<String, String> headers;
-
+public enum HttpMethod {
+  GET,
+  HEAD,
+  POST,
+  DELETE,
+  PUT,
+  PATCH
 }
